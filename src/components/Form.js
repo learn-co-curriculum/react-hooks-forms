fa27257c966bb03ad63cba6e4cd7954ee107a013
Elvis-Munene <gitau.elvis@student.moringaschool.com>
2022-07-17 16:58:50 +0300

    commit tree 27131c63a9b968773aa67398c530ca59f0c3853e

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,27 @@
 import React, { useState } from "react";
 
 function Form() {
-  const [firstName, setFirstName] = useState("John");
-  const [lastName, setLastName] = useState("Henry");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [newsLetter, setNewsLetter] = useState(false);
+ function handleFirstNameChange(event) {
+setFirstName(event.target.value);
+ }
+ function handleLastNameChange(event) {
+  setLastName(event.target.value);
+   }
+   function handleNewsLetterChange(event) {
+    setNewsLetter(event.target.checked);
+
+   }
+
+
 
   return (
     <form>
-      <input type="text" value={firstName} />
-      <input type="text" value={lastName} />
+      <input type="text" onChange={handleFirstNameChange} value={firstName} />
+      <input type="text" onChange={handleLastNameChange} value={lastName} />
+      <input type="checkbox" id="newsletter" onChange={handleNewsLetterChange} checked={newsLetter} />
       <button type="submit">Submit</button>
     </form>
   );
